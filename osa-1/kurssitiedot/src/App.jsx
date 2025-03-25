15d888c14409ff9634f87b113751f26a944628ed
JV <Maahantuoja@gmail.com>
2025-03-25 18:27:55 +0200

    1.6: unicafe step1 | Done

--- osa-1/kurssitiedot/src/App.jsx
+++ osa-1/kurssitiedot/src/App.jsx
@@ -1,67 +1,61 @@
+/* eslint-disable no-unused-vars */
+import React, { useState } from 'react';
+
 const Header = (props) => {
   return (
     <>
       <h1>
-        {props.course}
+        {props.text}
       </h1>
     </>
   )
 }
 
-const Part = (props) => {
-  return (
-    <p>
-      {props.name} {props.exercises}
-    </p>
-  )
-}
-
-const Content = (props) => {
+const Button = ({ onClick, text}) => {
   return (
     <>
-      {props.parts.map((part, index) => (
-        <Part key={index} name={part.name} exercises={part.exercises} />
-      ))}
-    </>
-  )
-}
-
-const Total = (props) => {
-  let total = 0
-  props.parts.forEach(part => {
-    total += part.exercises
-  })
-  return (
-    <>
-      <p>
-        Number of exercises {total}
-      </p>
+      <button onClick={onClick}>
+        {text}
+      </button>
     </>
   )
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = 'Give feedback'
+  const statistics = 'Statistics'
+
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleGoodClick = () => {
+    setGood(good + 1)
+  }
+
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1)
+  }
+
+  const handleBadClick = () => {
+    setBad(bad + 1)
+  }
 
   return (
     <>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Header text={course} />
+
+      <Button onClick={handleGoodClick} text='Good' />{' '}
+      <Button onClick={handleNeutralClick} text='Neutral' />{' '}
+      <Button onClick={handleBadClick} text='Bad' />
+
+      <Header text={statistics} />
+
+      <p>
+        Good {good}<br />
+        Neutral {neutral}<br />
+        Bad {bad}
+      </p>
     </>
   )
 }

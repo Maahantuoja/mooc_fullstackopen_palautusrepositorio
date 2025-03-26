717251ed887ad4684711eeea1a5aff8a6c06060c
JV <Maahantuoja@gmail.com>
2025-03-26 19:24:01 +0200

    Lisätty keskiarvon laskeminen

--- osa-1/kurssitiedot/src/App.jsx
+++ osa-1/kurssitiedot/src/App.jsx
@@ -1,8 +1,8 @@
 /* eslint-disable no-unused-vars */
 import React, { useState } from 'react';
 
-const Header = (props) => (
-  <h1>{props.text}</h1>
+const Header = ({ text }) => (
+  <h1>{text}</h1>
 )
 
 const Button = ({ onClick, text }) => (
@@ -18,10 +18,39 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
+  const [average, setAverage] = useState(0)
+  // const [positive, setPositive] = useState(0)
 
-  const handleGoodClick = () => setGood(good + 1);
-  const handleNeutralClick = () => setNeutral(neutral + 1);
-  const handleBadClick = () => setBad(bad + 1);
+  const handleGoodClick = () => {
+    const updateGood = good + 1
+    const updateAll = updateGood + neutral + bad
+    const updateAverage = (updateGood - bad) / updateAll
+
+    setGood(updateGood)
+    setAll(updateAll)
+    setAverage(updateAverage)
+  }
+
+  const handleNeutralClick = () => {
+    const updateNeutral = neutral + 1
+    const updateAll = good + updateNeutral + bad
+    const updateAverage = (good - bad) / updateAll
+
+    setNeutral(updateNeutral)
+    setAll(updateAll)
+    setAverage(updateAverage)
+  }
+  
+  const handleBadClick = () => {
+    const updateBad = bad + 1
+    const updateAll = good + neutral + updateBad
+    const updateAverage = (good - updateBad) / updateAll
+
+    setBad(updateBad)
+    setAll(updateAll)
+    setAverage(updateAverage)
+  }
 
   return (
     <>
@@ -34,9 +63,12 @@ const App = () => {
       <Header text={statistics} />
 
       <p>
-        Good {good}<br />
-        Neutral {neutral}<br />
-        Bad {bad}
+        Good: {good}<br />
+        Neutral: {neutral}<br />
+        Bad: {bad}<br />
+        All: {all}<br />
+        Average: {average}<br />
+        {/* Positive: {positive}<br /> */}
       </p>
     </>
   )

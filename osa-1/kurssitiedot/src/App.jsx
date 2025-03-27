a16d5f7a916caf0294431e41d604883af17adbdb
JV <Maahantuoja@gmail.com>
2025-03-27 09:52:51 +0200

    Hyvän palautteen prosenttiosuus

--- osa-1/kurssitiedot/src/App.jsx
+++ osa-1/kurssitiedot/src/App.jsx
@@ -14,42 +14,47 @@ const Button = ({ onClick, text }) => (
 const App = () => {
   const course = 'Give feedback'
   const statistics = 'Statistics'
-
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
   const [all, setAll] = useState(0)
   const [average, setAverage] = useState(0)
-  // const [positive, setPositive] = useState(0)
+  const [positive, setPositive] = useState(0)
 
   const handleGoodClick = () => {
     const updateGood = good + 1
     const updateAll = updateGood + neutral + bad
     const updateAverage = (updateGood - bad) / updateAll
+    const updatePositive = updateGood / updateAll * 100
 
     setGood(updateGood)
     setAll(updateAll)
     setAverage(updateAverage)
+    setPositive(updatePositive)
   }
 
   const handleNeutralClick = () => {
     const updateNeutral = neutral + 1
     const updateAll = good + updateNeutral + bad
     const updateAverage = (good - bad) / updateAll
+    const updatePositive = good / updateAll * 100
 
     setNeutral(updateNeutral)
     setAll(updateAll)
     setAverage(updateAverage)
+    setPositive(updatePositive)
   }
   
   const handleBadClick = () => {
     const updateBad = bad + 1
     const updateAll = good + neutral + updateBad
     const updateAverage = (good - updateBad) / updateAll
+    const updatePositive = good / updateAll * 100
 
     setBad(updateBad)
     setAll(updateAll)
     setAverage(updateAverage)
+    setPositive(updatePositive)
   }
 
   return (
@@ -68,7 +73,7 @@ const App = () => {
         Bad: {bad}<br />
         All: {all}<br />
         Average: {average}<br />
-        {/* Positive: {positive}<br /> */}
+        Positive: {positive} %<br />
       </p>
     </>
   )

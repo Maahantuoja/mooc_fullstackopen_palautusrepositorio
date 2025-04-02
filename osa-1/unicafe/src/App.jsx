9cbbf4d90ddedbc3ac3bbf51704fef513ea42b05
JV <Maahantuoja@gmail.com>
2025-04-02 12:23:41 +0300

    1.6: unicafe step1 | Done

---
+++ osa-1/unicafe/src/App.jsx
@@ -0,0 +1,63 @@
+/* eslint-disable no-unused-vars */
+import React, { useState } from 'react';
+
+const Header = (props) => {
+  return (
+    <>
+      <h1>
+        {props.text}
+      </h1>
+    </>
+  )
+}
+
+const Button = ({ onClick, text }) => {
+  return (
+    <>
+      <button onClick={onClick}>
+        {text}
+      </button>
+    </>
+  )
+}
+
+const App = () => {
+  const course = 'Give feedback'
+  const statistics = 'Statistics'
+
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleGoodClick = () => {
+    setGood(good + 1)
+  }
+
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1)
+  }
+
+  const handleBadClick = () => {
+    setBad(bad + 1)
+  }
+
+  return (
+    <>
+      <Header text={course} />
+
+      <Button onClick={handleGoodClick} text='Good' />{' '}
+      <Button onClick={handleNeutralClick} text='Neutral' />{' '}
+      <Button onClick={handleBadClick} text='Bad' />
+
+      <Header text={statistics} />
+
+      <p>
+        Good {good}<br />
+        Neutral {neutral}<br />
+        Bad {bad}
+      </p>
+    </>
+  )
+}
+
+export default App

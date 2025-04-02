8ca010d0eb1d681f1a0ee65c9800e753ef0d121d
JV <Maahantuoja@gmail.com>
2025-04-02 12:38:28 +0300

    1.10: unicafe step5 | done

--- osa-1/unicafe/src/App.jsx
+++ osa-1/unicafe/src/App.jsx
@@ -11,21 +11,29 @@ const Button = ({ onClick, text }) => (
   </button>
 )
 
+const StatisticLine = ({ text, value }) => (
+  <>
+    {text}: {value}<br />
+  </>
+
+)
+
 const Statistics = (props) => {
   if (props.all === 0) {
     return (
       <p>No feedback given</p>
     )
   }
+
   return (
-    <p>
-      Good: {props.good}<br />
-      Neutral: {props.neutral}<br />
-      Bad: {props.bad}<br />
-      All: {props.all}<br />
-      Average: {props.average}<br />
-      Positive: {props.positive} %<br />
-    </p>
+    <>
+      <StatisticLine text="Good" value={props.good} />
+      <StatisticLine text="Neutral" value={props.neutral} />
+      <StatisticLine text="Bad" value={props.bad} />
+      <StatisticLine text="All" value={props.all} />
+      <StatisticLine text="Average" value={props.average} />
+      <StatisticLine text="Positive" value={props.positive + " %"} />
+    </>
   )
 }
 

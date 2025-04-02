58d280edda9aa4a641c9a016a0dda52a44d4de2b
JV <Maahantuoja@gmail.com>
2025-04-02 12:35:09 +0300

    1.6: unicafe step1

--- osa-1/unicafe/src/App.jsx
+++ osa-1/unicafe/src/App.jsx
@@ -1,44 +1,60 @@
 /* eslint-disable no-unused-vars */
 import React, { useState } from 'react';
 
-const Header = (props) => {
-  return (
-    <>
-      <h1>
-        {props.text}
-      </h1>
-    </>
-  )
-}
+const Header = ({ text }) => (
+  <h1>{text}</h1>
+)
 
-const Button = ({ onClick, text }) => {
-  return (
-    <>
-      <button onClick={onClick}>
-        {text}
-      </button>
-    </>
-  )
-}
+const Button = ({ onClick, text }) => (
+  <button onClick={onClick}>
+    {text}
+  </button>
+)
 
 const App = () => {
   const course = 'Give feedback'
   const statistics = 'Statistics'
-
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
+  const [average, setAverage] = useState(0)
+  const [positive, setPositive] = useState(0)
 
   const handleGoodClick = () => {
-    setGood(good + 1)
+    const updateGood = good + 1
+    const updateAll = updateGood + neutral + bad
+    const updateAverage = (updateGood - bad) / updateAll
+    const updatePositive = updateGood / updateAll * 100
+
+    setGood(updateGood)
+    setAll(updateAll)
+    setAverage(updateAverage)
+    setPositive(updatePositive)
   }
 
   const handleNeutralClick = () => {
-    setNeutral(neutral + 1)
+    const updateNeutral = neutral + 1
+    const updateAll = good + updateNeutral + bad
+    const updateAverage = (good - bad) / updateAll
+    const updatePositive = good / updateAll * 100
+
+    setNeutral(updateNeutral)
+    setAll(updateAll)
+    setAverage(updateAverage)
+    setPositive(updatePositive)
   }
 
   const handleBadClick = () => {
-    setBad(bad + 1)
+    const updateBad = bad + 1
+    const updateAll = good + neutral + updateBad
+    const updateAverage = (good - updateBad) / updateAll
+    const updatePositive = good / updateAll * 100
+
+    setBad(updateBad)
+    setAll(updateAll)
+    setAverage(updateAverage)
+    setPositive(updatePositive)
   }
 
   return (
@@ -52,9 +68,12 @@ const App = () => {
       <Header text={statistics} />
 
       <p>
-        Good {good}<br />
-        Neutral {neutral}<br />
-        Bad {bad}
+        Good: {good}<br />
+        Neutral: {neutral}<br />
+        Bad: {bad}<br />
+        All: {all}<br />
+        Average: {average}<br />
+        Positive: {positive} %<br />
       </p>
     </>
   )
